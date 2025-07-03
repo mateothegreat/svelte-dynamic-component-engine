ce4b7e67673d77d81c9ba5b16fa2f386eec78295
Matthew Davis <matthew@matthewdavis.io>
2025-07-03 14:25:27 -0500

    initial commit

--- src/version.ts
+++ src/version.ts
@@ -13,6 +13,21 @@ export type Version = {
 };
 
 export const getVersion = (): Version => {
-  const version = JSON.parse(import.meta.env.VERSION);
-  return version;
+  if (!import.meta?.env?.VERSION) {
+    console.error("VERSION is not set");
+    return {
+      tag: "unknown",
+      commit: {
+        long: "unknown",
+        short: "unknown"
+      },
+      dirty: true,
+      branch: "unknown",
+      date: {
+        actual: new Date(1970, 1, 1),
+        human: "unknown"
+      }
+    };
+  }
+  return JSON.parse(import.meta.env.VERSION) as Version;
 };
