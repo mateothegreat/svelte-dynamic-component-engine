f840c14177ff517f690aea4972bf2ec40defe9bf
Matthew Davis <matthew@matthewdavis.io>
2025-07-03 14:21:24 -0500

    initial commit

--- vite-plugin-version.ts
+++ vite-plugin-version.ts
@@ -1,4 +1,4 @@
-import { execSync, spawnSync } from "child_process";
+import { spawnSync } from "child_process";
 import type { Plugin } from "vite";
 
 export const ago = (date: Date, locale = "en"): string => {
@@ -27,32 +27,63 @@ export const ago = (date: Date, locale = "en"): string => {
   return rtf.format(0, "second");
 };
 
-export default function versionPlugin(): Plugin {
+export const run = (
+  command: string
+): {
+  output: string;
+  status: number | null;
+} => {
+  const result = spawnSync(command, {
+    cwd: process.cwd(),
+    shell: true
+  });
+  if (result.status !== 0) {
+    return {
+      output: result.stderr.toString().trim(),
+      status: result.status
+    };
+  }
+  return {
+    output: result.stdout.toString().trim(),
+    status: result.status
+  };
+};
+
+export const git = {
+  status: () => run("git status").output,
+  tag: () => run("git describe --tags --abbrev=0").output,
+  fetch: () => run("git fetch --all -v ").output,
+  log: () =>
+    run(
+      "git log --graph -10 --branches --remotes --tags  --format=format:'%Cgreen%h %Creset• %s (%cN, %cr) %Cred%d' --date-order"
+    ),
+  date: () => run("git log -1 --format=%cd").output,
+  commit: {
+    long: () => run("git rev-parse HEAD").output,
+    short: () => run("git rev-parse --short HEAD").output
+  },
+  branch: () => run("git rev-parse --abbrev-ref HEAD").output,
+  dirty: () => run("git diff --quiet").status !== 0
+};
+
+export default (): Plugin => {
   return {
     name: "version-plugin",
     applyToEnvironment(env) {
-      console.log(execSync("git status").toString());
-      console.log(execSync("git tag -l").toString());
-      console.log(execSync("git fetch --all -v ").toString());
-      console.log(
-        execSync(
-          "git log --graph -10 --branches --remotes --tags  --format=format:'%Cgreen%h %Creset• %s (%cN, %cr) %Cred%d' --date-order"
-        ).toString()
-      );
       env.config.env.VERSION = JSON.stringify({
         date: {
-          actual: new Date(execSync("git log -1 --format=%cd").toString()),
-          human: ago(new Date(execSync("git log -1 --format=%cd").toString()))
+          actual: new Date(git.date()),
+          human: ago(new Date(git.date()))
         },
-        tag: execSync("git describe --tags --abbrev=0").toString().trim(),
+        tag: git.tag(),
         commit: {
-          long: execSync("git rev-parse HEAD").toString().trim(),
-          short: execSync("git rev-parse --short HEAD").toString().trim()
+          long: git.commit.long(),
+          short: git.commit.short()
         },
-        dirty: spawnSync("git diff --quiet").status !== 0,
-        branch: execSync("git rev-parse --abbrev-ref HEAD").toString().trim()
+        dirty: git.dirty(),
+        branch: git.branch()
       });
       return true;
     }
   };
-}
+};
